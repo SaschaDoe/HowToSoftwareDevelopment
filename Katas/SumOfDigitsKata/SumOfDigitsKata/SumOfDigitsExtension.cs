873c7db440df591cb23635fa1757c8cc5fbcb3dd
SaschaDoe <sascha.doerflein@web.de>
2021-01-11 21:35:31 +0100

    Add SumOfDigitsFunctional (but not ideal)

--- Katas/SumOfDigitsKata/SumOfDigitsKata/SumOfDigitsExtension.cs
+++ Katas/SumOfDigitsKata/SumOfDigitsKata/SumOfDigitsExtension.cs
@@ -29,29 +29,41 @@ namespace SumOfDigitsKata
             return sum;
         }
 
-        public static IEnumerable<int> ParseStringToInt(string input)
+        public static IEnumerable<int> ParseStringToIntSequence(int input)
         {
-            return input.Select(t => int.Parse(t.ToString()));
-        }
-        
-        public static int SumOfDigitsFunctional(this int digits)
-        {
-            var text = digits.ToString();
-            var numberOfDigits = text.Length;
-            var sum = digits;
-            while (numberOfDigits > 1)
-            {
-                
-                text = sum.ToString();
-                var numbers = ParseStringToInt(text);
-                sum = numbers.Sum();
-                var sumText = sum.ToString();
-                numberOfDigits = sumText.Length;
-            }
-            
-            return sum;
+            return input.ToString().Select(t => int.Parse(t.ToString()));
         }
 
-       
+        public static int NumberOfDigits(this int number)
+        {
+            return (int) Math.Floor(Math.Log10(number) + 1);
+        }
+
+        public static int SumOfDigitsFunctional(this int digits)
+        {
+            return digits.NumberOfDigits() == 1
+                ? digits
+                : SumOfDigitsSequence(digits).TakeWhile(x => x.numberOfDigits > 1).ToList().Count > 0
+                    ? SumOfDigitsSequence(digits).TakeWhile(x => x.numberOfDigits > 1).ToList().Last().sum
+                    : SumOfDigitsSequence(digits).Take(1).ToList().Last().sum;
+        }
+
+        private static IEnumerable<(int numberOfDigits,int sum )> SumOfDigitsSequence(int digits)
+        {
+            var sum = digits;
+            while (true)
+            {
+                var numberOfDigits = SumOfDigitsStep(ref sum);
+                yield return new (numberOfDigits, sum);
+            }
+        }
+
+        private static int SumOfDigitsStep(ref int sum)
+        {
+            int numberOfDigits;
+            sum = ParseStringToIntSequence(sum).Sum();
+            numberOfDigits = NumberOfDigits(sum);
+            return numberOfDigits;
+        }
     }
 }
